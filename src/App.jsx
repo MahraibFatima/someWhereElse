39250a924a354a19c88b8cbdd605b461662f63e4
MahraibFatima <mahraibfatima5@gmail.com>
2025-11-30 01:28:34 +0500

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Navbar from './components/navbar.jsx';
 import Hero from './components/hero.jsx';
+import Contact from './components/contact.jsx';
+
 function App() {
 
   return (
     <>
       {/* <Navbar/> */}
+      {/* <Contact/> */}
       <Hero />
+
     </>
   )
 }

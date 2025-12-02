732f2c0377ddae53b2e0429e5ec92cd89eeaf427
MahraibFatima <mahraibfatima5@gmail.com>
2025-12-02 21:00:03 +0500

    service bg added.

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,6 +1,14 @@
 const Service = () => {
     return (
-        <div>Service Page</div>
+        <div className="relative w-full h-screen">
+            <div className="absolute inset-0 z-0">
+                <img
+                    src="/assets/5.jpg"
+                    alt="Service Background"
+                    className="w-full h-full object-cover"
+                />
+                <div className="absolute inset-0 bg-opacity-40"></div>
+            </div></div>
     );
 };
 

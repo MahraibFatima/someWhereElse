9c4d68602e97cf4befe33add46f766e8512e726c
MahraibFatima <mahraibfatima5@gmail.com>
2025-12-02 19:24:15 +0500

    service page and about page route added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css'
-import Navbar from './components/navbar.jsx';
 import Hero from './components/hero.jsx';
 import Contact from './components/contact.jsx';
+import About from './components/about.jsx';
+import Services from './components/services.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
     <div>
 
-      <BrowserRouter>
+      <BrowserRouter>  
         <Routes>
           <Route path='/' element={<Hero />} />
+          <Route path='/about' element={<About />} />
+          <Route path= '/services' element={<Services />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
 

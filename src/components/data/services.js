070fff5459a5559532c2e115deda33f01b9e6ea9
MahraibFatima <mahraibfatima5@gmail.com>
2025-12-09 13:23:52 +0500

    places data added.

--- src/components/data/services.js
+++ src/components/data/services.js
@@ -1,4 +1,4 @@
-const servicesData = [
+export const foodData = [
     {
         img: "assets/food/1.jpg",
         title: "Street Food",
@@ -42,5 +42,49 @@ const servicesData = [
         description: "Fresh Caesar Shake with fvrt homemade biscuits."
     }
 ];
+export const placesData = [
+    {
+        img: "assets/places/1.jpg",
+        title: "Desert Moutain",
+        rating: "4.9",
+        price: "$299.99",
+        description: "Experience the serene beauty of desert landscapes with our exclusive package."
+    },
+    {
+        img: "assets/places/2.jpg",
+        title: "Sunset bridge",
+        rating: "4.8",
+        price: "$349.99",
+        description: "Enjoy breathtaking sunset views from iconic bridges around the world."
+    },
+    {
+        img: "assets/places/3.jpg",
+        title: "Hiking Trip",
+        rating: "4.7",
+        price: "$399.99",
+        description: "Join us for an adventurous hiking trip through scenic trails and mountains."
+    },
+    {
+        img: "assets/places/4.jpg",
+        title: "Nature Beauty",
+        rating: "4.9",
+        price: "$299.99",
+        description: "Immerse yourself in the stunning beauty of nature with our guided tours."
+    },
+    {
+        img: "assets/places/5.jpg",
+        title: "Beach Side",
+        rating: "4.7",
+        price: "$249.99",
+        description: "Wake up to the sound of gentle waves at this stunning oceanfront paradise. Enjoy direct beach access, breathtaking sunset views, and luxurious amenities just steps from the water's edge."
+    },
+    {
+        img: "assets/places/6.jpg",
+        title: "Rain Forest Retreat",
+        rating: "4.7",
+        price: "$399.99",
+        description: "Immerse yourself in nature at this secluded jungle sanctuary. Surrounded by lush tropical foliage, exotic wildlife, and serene waterfalls for the ultimate eco-friendly getaway."
+    }
+];
 
-export default servicesData;
+export const servicesData = [...foodData, ...placesData];
